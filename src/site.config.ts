d1c60633865c703d79a37ceb4f5b032601f54d4d
zyb <384201115@qq.com>
2025-01-25 11:37:12 +0800

    add

--- src/site.config.ts
+++ src/site.config.ts
@@ -3,19 +3,23 @@ import type { CardListData, Config, IntegrationUserConfig, ThemeUserConfig } fro
 export const theme: ThemeUserConfig = {
   // === Basic configuration ===
   /** Title for your website. Will be used in metadata and as browser tab title. */
-  title: 'haowuyiwai',
+  title: 'z_cloud_bo\'s blog',
   /** Will be used in index page & copyright declaration */
-  author: 'Pure Lab',
+  author: 'z_cloud_bo',
   /** Description metadata for your website. Can be used in page metadata. */
   description: 'Stay hungry, stay foolish',
   /** The default favicon for your site which should be a path to an image in the `public/` directory. */
   favicon: '/favicon/favicon.ico',
   /** Specify the default language for this site. */
   locale: {
-    lang: 'en-US',
-    attrs: 'en_US',
+    // lang: 'en-US',
+    // attrs: 'en_US',
+    // // Date locale
+    // dateLocale: 'en-US',
+    lang: 'zh-CN',
+    attrs: 'zh-CN',
     // Date locale
-    dateLocale: 'en-US',
+    dateLocale: 'zh-CN',
     dateOptions: {
       day: 'numeric',
       month: 'short',
